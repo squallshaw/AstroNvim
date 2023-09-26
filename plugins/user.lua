b415cca2a43e09eb8d6289efca510aae7c6fee81
squallshaw <squallshaw411@gmail.com>
2023-09-26 11:10:32 +0800

    Remove csharpls-extension

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,7 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  "Decodetalkers/csharpls-extended-lsp.nvim",
+  -- "Decodetalkers/csharpls-extended-lsp.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
   --   event = "BufRead",
